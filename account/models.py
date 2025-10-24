f10ea8ffe04a9ec4f8e362bc1a9104055039d850
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2025-10-24 11:40:55 +0700

    syncing account, detail_product, and review

--- account/models.py
+++ account/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from detail_product.models import Product
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
     nickname = models.CharField(max_length=30, blank=True, null=True)
     age = models.PositiveIntegerField(blank=True, null=True)
     profile_picture = models.URLField(blank=True, null=True)  # simpan URL gambar
-    # favorites = models.ManyToManyField(Product, related_name='favorited_by', blank=True)
+    favorites = models.ManyToManyField(Product, related_name='favorited_by', blank=True)
 
 
     def __str__(self):
