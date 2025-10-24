0df6e5ff4fddfa38d0b0ca0d8e9e15538c752f19
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-10-24 23:01:14 +0700

    Implementasi homepage dasar sori banget baru commit

--- Homepage/views.py
+++ Homepage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from detail_product.models import Product
+from .models import SliderProduct
 
-# Create your views here.
+def show_main(request):
+    product_catalog = Product.objects.all() #TODO
+
+    slider_products = SliderProduct.objects.all()
+
+    context = {
+        'product_catalog': product_catalog,
+        'slider_products': slider_products,
+    }
+    return render(request, "homepage.html", context)
