00da7572c8345ce2643d7d37272f6e7f7bd1f3c1
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2025-10-26 22:23:02 +0700

    mengubah penggunaan regex pada saat proses filtering

--- Homepage/views.py
+++ Homepage/views.py
@@ -1,3 +1,5 @@
+# Homepage/views.py
+
 from django.shortcuts import render
 from detail_product.models import Product
 from .models import SliderProduct
@@ -7,25 +9,20 @@ def show_main(request):
     product_catalog = Product.objects.all()
     slider_products = SliderProduct.objects.all()
 
-    # Ambil kata kunci filter dari URL, contoh: /?q=men
     query = request.GET.get('q')
 
     if query:
-        # Ubah query menjadi huruf kecil untuk konsistensi
         query = query.lower()
         if query == 'men':
-            # Jika query 'men', tampilkan produk 'men' ATAU 'unisex'
             product_catalog = product_catalog.filter(
-                Q(name__iregex=r'\bmen\b') | Q(name__iregex=r'\bunisex\b')
+                Q(name__icontains='men') | Q(name__icontains='unisex')
             )
         elif query == 'women':
-            # Jika query 'women', tampilkan produk 'women' ATAU 'unisex'
             product_catalog = product_catalog.filter(
-                Q(name__iregex=r'\bwomen\b') | Q(name__iregex=r'\bunisex\b')
+                Q(name__icontains='women') | Q(name__icontains='unisex')
             )
         else:
-            # Untuk query lain seperti 'kids', cari kata kunci yang cocok
-            product_catalog = product_catalog.filter(name__iregex=r'\b{}\b'.format(query))
+            product_catalog = product_catalog.filter(name__icontains=query)
 
     context = {
         'product_catalog': product_catalog,
