047c0521c0d554cb5780b13f9a7237b53c49ef73
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-10-26 23:54:52 +0700

    Added Slider Product Form

--- Homepage/admin.py
+++ Homepage/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import SliderProduct
+from .forms import SliderProductForm
 
-# Register your models here.
+@admin.register(SliderProduct)
+class SliderProductAdmin(admin.ModelAdmin):
+    form = SliderProductForm
+    list_display = ('product', 'is_featured', 'discount', 'average_rating_display')
+    list_filter = ('is_featured',)
+    search_fields = ('product__name',)
+    list_editable = ('is_featured', 'discount')
+
+    def average_rating_display(self, obj):
+        return f"{obj.average_rating:.1f}"
+    average_rating_display.short_description = "Avg Rating"
+
--- Homepage/views.py
+++ Homepage/views.py
@@ -1,8 +1,9 @@
 # Homepage/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from detail_product.models import Product
 from .models import SliderProduct
+from .forms import SliderProductForm
 from django.db.models import Q
 
 def show_main(request):
@@ -29,3 +30,26 @@ def show_main(request):
         'slider_products': slider_products,
     }
     return render(request, "homepage.html", context)
+
+def manage_slider(request):
+    slider_products = SliderProduct.objects.all()
+
+    if request.method == 'POST':
+        form = SliderProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Homepage:manage_slider')
+        
+    else:
+        form = SliderProductForm
+
+    context = {
+        'form': form,
+        'slider_products': slider_products,
+    }
+    return render(request, 'manage_slider.html', context)
+
+def delete_slider_product(request, id):
+    product = get_object_or_404(SliderProduct, id=id)
+    product.delete()
+    return redirect('Homepage:manage_slider')
