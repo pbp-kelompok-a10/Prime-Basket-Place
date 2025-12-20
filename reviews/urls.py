474f4f8593a36a2b6c3cb298b33451f00e70ff0e
0xArsy <zarsy11000@gmail.com>
2025-12-20 16:14:16 +0700

    Integration Review Modul

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('create/<int:product_id>/', views.create_review, name='create_review'),
     path('update/<int:pk>/', views.update_review, name='update_review'),
     path('delete/<int:pk>/', views.delete_review, name='delete_review'),
+
+    path('json/<int:product_id>/', views.get_reviews_json, name='get_reviews_json'),
+    path('json/<int:product_id>/stats/', views.get_review_stats_json, name='get_review_stats_json'),
+    path('create-flutter/<int:product_id>/', views.create_review_flutter, name='create_review_flutter'),
 ]
